47af78675a6d9531a098cd8f65f69fbf9f918fee
= <=>
2017-06-19 16:37:12 +0700

    update avg hsl

--- FileExample.java
+++ FileExample.java
@@ -1,10 +1,15 @@
 package FileExample;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
+import java.io.BufferedReader; // doc file
+import java.io.FileInputStream; // doc file
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.FileOutputStream; // ghi file
+import java.io.IOException; 
+import java.io.InputStreamReader; // doc file
+import java.io.OutputStreamWriter; // ghi file
+import java.io.PrintWriter; //ghi file
+import java.util.ArrayList; // array list
+import java.util.Random; // random
 
 public class FileExample {
 	
@@ -13,23 +18,44 @@ public class FileExample {
 			FileInputStream fis = new FileInputStream("test.txt");
 			InputStreamReader isr = new InputStreamReader(fis);
 			BufferedReader br = new BufferedReader(isr);
-		
+			
+			ArrayList <Double> HSLList 	= new ArrayList<>();
+			ArrayList <String> NameList = new ArrayList<>(); 
+			
+			Double hsl;
+			double TotalHsl = 0;
+			
 			while (true) {
 				try {
 					String st = br.readLine();
 					if(st == null || st == "") break;
-					System.out.println(st);
+					
+					String [] result = st.split("[;]");
+					NameList.add(result[1]);
+					
+					hsl = Double.parseDouble(result[2]);
+					
+					TotalHsl += hsl;
+					
+					HSLList.add(hsl);
+					
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
+			
+			for (int i = 0; i < NameList.size(); i++) {
+				System.out.println(i + " - " + NameList.get(i) + " - " + HSLList.get(i));
+			}
+			System.out.println("Trung binh He So Luong: " + TotalHsl/NameList.size());
+			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-
+	
 	public void WriteFile () {
 		try {
 			FileOutputStream fos = new FileOutputStream("test.txt", true); // mở file
